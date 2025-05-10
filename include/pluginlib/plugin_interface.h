7ce31d202c286e06e252168791ecfb5510f9c1f1
coder199608 <yangruitao6@gmail.com>
2025-05-10 09:26:53 +0800

    plugin interface factor and macro

--- include/pluginlib/plugin_interface.h
+++ include/pluginlib/plugin_interface.h
@@ -6,6 +6,7 @@
 #define PLUGIN_INTERFACE_H
 
 #include <string>
+#include <memory>
 
 namespace pluginlib {
     class PluginInterface {
@@ -17,6 +18,19 @@ virtual std::string description() const = 0;
 vitual std::string author() const = 0;
 virtual bool initialize() = 0;
     };
+
+using PluginInterfacePtr = std::shared_ptr<PluginInterface>;
+
+    template<typename T>
+    class PluginFactory {
+        public:
+            virtual ~PluginFactory() = default;
+            virtual std::shared_ptr<T> create() = 0;
+            virtual void destroy(std::shared_ptr<T> instance) = 0;
+    };
+
+    template<typename T>
+    using PluginFactoryPtr = std::shared_ptr<PluginFactory<T>>;
 } // namespace pluginlib
 
 #endif //PLUGIN_INTERFACE_H
---
+++ include/pluginlib/plugin_macro.h
@@ -0,0 +1,24 @@
+//
+// Created by ryan on 2025/5/10.
+//
+
+#ifndef PLUGIN_MACRO_H
+#define PLUGIN_MACRO_H
+
+#include "plugin_interface.h"
+
+#define REGISTER_PLUGIN_FACTORY(CLASS) \
+class CLASS##Factory : public pluginlib::PluginFactory<pluginlib::PluginInterface> { \
+public: \
+pluginlib::PluginInterfacePtr create() override { \
+return std::make_shared<CLASS>(); \
+} \
+void destroy(pluginlib::PluginInterfacePtr instance) override { \
+instance.reset(); \
+} \
+}; \
+extern "C" pluginlib::PluginFactoryPtr<pluginlib::PluginInterface> create_plugin_factory() { \
+return std::make_shared<CLASS##Factory>(); \
+}
+
+#endif //PLUGIN_MACRO_H
