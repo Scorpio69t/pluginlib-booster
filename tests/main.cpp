78270fbd9ffad4e462e88952deff3981415a444f
yangruitao <18728163854@163.com>
2025-05-12 11:27:50 +0800

    format

--- tests/main.cpp
+++ tests/main.cpp
@@ -9,7 +9,7 @@ int main() {
     pluginlib::PluginManager manager;
     manager.load_plugins("./plugins");
 
-    if (auto logger = manager.get_plugin<pluginlib::PluginInterface>("Logger")) {
+    if (const auto logger = manager.get_plugin<pluginlib::PluginInterface>("Logger")) {
         std::cout << "[Main] Plugin loaded: " << logger->name() << std::endl;
     }
 
